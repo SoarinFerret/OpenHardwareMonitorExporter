283581283241b0717abb0106c9663c5b1c83f51d
Cody Ernesti <thecodye@kanto.cloud>
2022-01-13 01:12:48 -0600

    Allow for additional sensor types

--- OpenHardwareMonitorExporter/MetricsVisitor.cs
+++ OpenHardwareMonitorExporter/MetricsVisitor.cs
@@ -26,6 +26,42 @@ namespace OpenHardwareMonitorExporter
         {
         }
 
+        private string SensorTypeToHelp(SensorType sensorType)
+        {
+            switch (sensorType)
+            {
+                case SensorType.Voltage:
+                    return "Voltage [V]";
+                case SensorType.Flow:
+                    return "Flow [L/h]";
+                case SensorType.Level:
+                    return "Level [%}";
+                case SensorType.Factor:
+                    return "Factor";
+                case SensorType.Clock:
+                    return "Clock [MHz]";
+                case SensorType.Temperature:
+                    return "Temperature [C]";
+                case SensorType.Load:
+                    return "Load [%]";
+                case SensorType.Control:
+                    return "Control [%]";
+                case SensorType.Fan:
+                    return "Fan [RPM]";
+                case SensorType.Power:
+                    return "Power consumption [W]";
+                case SensorType.Data:
+                    return "Data [GB]";
+                case SensorType.SmallData:
+                    return "Data [MB]";
+                case SensorType.Throughput:
+                    return "Throughput [MB/s]";
+                default:
+                    return "Unknown";
+            }
+        }
+
+
         public void VisitSensor(ISensor sensor)
         {
             if (sensor.Value != null)
@@ -34,22 +70,8 @@ namespace OpenHardwareMonitorExporter
                 {
                     var metricName = $"ohm_cpu_{sensor.SensorType.ToString().ToLowerInvariant()}";
                     var hw = sensor.Hardware.Identifier.ToString();
-                    var help = "TODO";
-                    switch (sensor.SensorType)
-                    {
-                        case SensorType.Clock:
-                            help = "Clock [MHz]";
-                            break;
-                        case SensorType.Load:
-                            help = "Load [%]";
-                            break;
-                        case SensorType.Temperature:
-                            help = "Temperature [C]";
-                            break;
-                        case SensorType.Power:
-                            help = "Power consumption [W]";
-                            break;
-                    }
+                    var help = SensorTypeToHelp(sensor.SensorType);
+
                     var gauge = Metrics.WithCustomRegistry(_registry).CreateGauge(metricName, help, "hw", "name");
                     gauge.Labels(hw, sensor.Name).Set(sensor.Value.Value);
                 }
